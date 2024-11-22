ae0197cc9910afac1590c8fd478f0512f357aaa0
Samantha <Troyandme04@gmail.com>
2024-11-21 21:48:15 -0500

    added motor setup

--- Joystick_Control/Joystick_Control.ino
+++ Joystick_Control/Joystick_Control.ino
@@ -230,9 +230,38 @@ void processControllers() {
   }
 }
 
-// Arduino setup function. Runs in CPU 1
+#include <Adafruit_NeoPixel.h> // Include the Adafruit NeoPixel library
+
+int* motor1Pin1 = 27;
+int* motor1Pin2 = 26;
+int* enable1Pin = 14;
+
+int* motor2Pin1 = 25;
+int* motor2Pin2 = 19;
+int* enable2Pin = 32;
+
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int* dutyCycle1;
+int* dutyCycle2;
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+  motor1Pin1 = 27;
+  motor1Pin2 = 26;
+  enable1Pin = 14;
+
+  motor2Pin1 = 25;
+  motor2Pin2 = 19;
+  enable2Pin = 32;
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+
+  ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel);
+
   Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
   const uint8_t* addr = BP32.localBdAddress();
   Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
